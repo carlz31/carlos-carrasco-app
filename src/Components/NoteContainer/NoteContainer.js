916e95696ef3c8640327dc3719cc045b59c3bacf
carlz31 <carlos.carrasco54@inacapmail.cl>
2022-07-26 20:59:50 -0400

    entregar evaluacion

---
+++ src/Components/NoteContainer/NoteContainer.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import Note from "../Note/Note";
+
+import "./NoteContainer.css";
+
+function NoteContainer(props) {
+  const reverArray = (arr) => {
+    const array = [];
+
+    for (let i = arr.length - 1; i >= 0; --i) {
+      array.push(arr[i]);
+    }
+
+    return array;
+  };
+
+  const notes = reverArray(props.notes);
+
+  return (
+    <div className="note-container">
+      <h1>Evaluacion 4 front end </h1>
+      <h2>Post It Simulator!</h2>
+      <h3> Agregar tareas</h3>
+      <div className="note-container_notes custom-scroll">
+        {notes?.length > 0 ? (
+          notes.map((item) => (
+            <Note
+              key={item.id}
+              note={item}
+              deleteNote={props.deleteNote}
+              updateText={props.updateText}
+            />
+          ))
+        ) : (
+          <h3>No tienes tareas por  hacer</h3>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default NoteContainer;
